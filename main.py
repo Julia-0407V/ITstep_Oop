e737c8af44435ec663f15237cb662e97e3c26c56
Julia-0407V <vasilchenkou@shc7svet.ukr.education>
2024-09-22 16:59:03 +0300

    1-st commit 2-st commit

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("Hello Dear B2123")
-print("CKB")
+"""
+class Student:
+    print("Hi")
+    def __init__(self):
+        self.height = 160
+        print ("I'm alive!")
+
+ivan = Student:
+"""
+"""
+class Student:
+    amount = 0
+    def __init__(self, height = 160):
+        self.height = height
+        Student.amount += 1
+ivan = Student()
+pavlo = Student(height = 170)
+print(ivan.amount)
+print(Student.amount)
+"""
+class Student:
+    height = 160
+    def __init__(self, height = 160):
+       print( self.height)
+       self.height += 10
+ivan = Student()
+pavlo = Student
